0dd8038ea386ced14e216078dc6ae20a3122fa26
smallH <472908522@qq.com>
2019-05-02 18:20:39 +0800

    更新

--- src/views/MainView.js
+++ src/views/MainView.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
-import ClipboardAPI from '../example/api/ClipboardAPI';
-import WebViewComp from '../example/comp/WebViewComp';
+import FetchAPI from '../example/api/FetchAPI';
 
 class MainView extends Component {
     render(){
         return (
             <View style={{flex:1}}>
-                <ClipboardAPI /> 
+                <FetchAPI /> 
             </View>
         );
     }
